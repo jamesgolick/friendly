076f193cbf2125fe4fbe3079ca9838c6cc6c50cf
James Golick <james@giraffesoft.ca>
2009-12-01 14:40:30 -0800

    add basic integration spec

---
+++ spec/integration/basic_object_lifecycle_spec.rb
@@ -0,0 +1,32 @@
+require File.expand_path("../../spec_helper", __FILE__)
+
+describe "Creating and retrieving an object" do
+  class IntegrationUser
+    include Friendly::Document
+
+    property :name, String
+    property :age,  Integer
+  end
+
+  before do
+    @user       = IntegrationUser.create :name => "Stewie Griffin",
+                                         :age  => 3
+    @found_user = IntegrationUser.find(@user.id)
+  end
+
+  it "finds the user in the database" do
+    @found_user.name.should == @user.name
+    @found_user.age.should  == @user.age
+    @found_user.id.should   == @user.id
+  end
+
+  it "locates an object that is == to the created object" do
+    @found_user.should == @user
+  end
+
+  it "sets the created_at timestamp for the record" do
+    @user.created_at.should_not be_nil
+    @user.created_at.should be_instance_of(Time)
+  end
+end
+
