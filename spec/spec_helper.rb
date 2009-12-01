eac3ccf34dcf639de61c8e96d016963b493d7546
James Golick <james@giraffesoft.ca>
2009-12-01 14:41:52 -0800

    depend on sequel

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rubygems'
 require 'friendly'
 require 'spec'
 require 'spec/autorun'
+require 'sequel'
 
 Spec::Runner.configure do |config|
   
