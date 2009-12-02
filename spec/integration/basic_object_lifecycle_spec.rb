38dc58132c42922a7f54efdbebbeeebbfce8fcfb
James Golick <james@giraffesoft.ca>
2009-12-02 10:51:14 -0800

    add integration spec for create vs update

--- spec/integration/basic_object_lifecycle_spec.rb
+++ spec/integration/basic_object_lifecycle_spec.rb
@@ -27,5 +27,45 @@ describe "Creating and retrieving an object" do
     @found_user.created_at.should_not be_nil
     @found_user.created_at.should == @user.created_at
   end
+
+  it "sets the updated_at" do
+    @user.updated_at.should == @user.created_at
+  end
+end
+
+describe "Updating an object" do
+  before do
+    @user = User.new :name => "Stewie Griffin",
+                     :age  => 3
+    @user.save
+    @created_id = @user.id
+    @created_at = @user.created_at
+
+    sleep(0.1)
+    @user.name = "Brian Griffin"
+    @user.age  = 8
+    @user.save
+
+    @found_user = User.find(@created_id)
+  end
+
+  it "sets the updated_at column" do
+    @user.updated_at.should_not be_nil
+    @user.updated_at.should_not == @user.created_at
+    @user.updated_at.should > @user.created_at
+  end
+
+  it "doesn't change the created_at" do
+    @user.created_at.should == @created_at
+  end
+
+  it "doesn't change the id" do
+    @user.id.should == @created_id
+  end
+
+  it "saves the new attrs to the db" do
+    @found_user.name.should == "Brian Griffin"
+    @found_user.age.should  == 8
+  end
 end
 
