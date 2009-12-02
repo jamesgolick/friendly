24b62875ec007a5bc1a34f0ee1a1d076fdbda66c
James Golick <james@giraffesoft.ca>
2009-12-01 17:05:59 -0800

    add config

---
+++ lib/friendly.rb
@@ -0,0 +1,13 @@
+require 'friendly/config'
+require 'friendly/document'
+
+module Friendly
+  class << self
+    def configure
+    end
+
+    def config
+      @config ||= Config.new
+    end
+  end
+end
