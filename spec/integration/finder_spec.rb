5af6d5dbc8b9ecec682cb88a07cdc118c3bb7d9a
James Golick <james@giraffesoft.ca>
2009-12-02 13:15:50 -0800

    raise when any objects aren't found with find()

--- spec/integration/finder_spec.rb
+++ spec/integration/finder_spec.rb
@@ -14,4 +14,20 @@ describe "Finding multiple objects by id" do
     @users.should include(@user_one)
     @users.should include(@user_two)
   end
+
+  describe "when no objects are found" do
+    it "raises RecordNotFound" do
+      lambda { 
+        User.find(99999999, 9999999, 99999) 
+      }.should raise_error(Friendly::RecordNotFound)
+    end
+  end
+
+  describe "when one object is found, but others aren't" do
+    it "raises RecordNotFound" do
+      lambda {
+        User.find(@user_one.id, 10000000)
+      }.should raise_error(Friendly::RecordNotFound)
+    end
+  end
 end
