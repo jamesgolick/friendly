74db3ea3251b996ea79d289c6bee64fde526b6fe
James Golick <james@giraffesoft.ca>
2009-12-04 10:18:58 -0800

    implement find many by id in finder

--- lib/friendly/finder.rb
+++ lib/friendly/finder.rb
@@ -7,10 +7,20 @@ module Friendly
       @translator = translator
     end
 
-    def find(klass, id)
+    def find(klass, *ids)
+      ids.length == 1 ? find_one(klass, ids.first) : find_many(klass, ids)
+    end
+
+    def find_one(klass, id)
       record = datastore.first(klass, :id => id)
       assert_record_found(record, klass, id)
       translator.to_object(klass, record)
+    end
+
+    def find_many(klass, ids)
+      records = datastore.all(klass, :id => ids)
+      assert_all_records_found(klass, records, ids)
+      records.map { |r| translator.to_object(klass, r) }
     end
 
     protected
@@ -21,6 +31,14 @@ module Friendly
       def assert_record_found(record, klass, id)
         if record.nil?
           raise RecordNotFound, "Couldn't find record #{klass.name}: #{id}."
+        end
+      end
+
+      def assert_all_records_found(klass, records, ids)
+        missing = ids - records.map { |r| r[:id] }
+        unless missing.empty?
+          missing = missing.join(",")
+          raise RecordNotFound, "Couldn't find records #{klass.name}: #{missing}."
         end
       end
   end
--- spec/unit/finder_spec.rb
+++ spec/unit/finder_spec.rb
@@ -35,4 +35,35 @@ describe "Friendly::Finder" do
       end
     end
   end
+
+  describe "finding multiple objects by id" do
+    before do
+      @records = [row(:id => 1), row(:id => 2), row(:id => 3)]
+      @records.each do |r|
+        @translator.stubs(:to_object).with(@klass, r).returns(@doc)
+      end
+    end
+    describe "when all the objects are found" do
+      before do
+        @datastore.stubs(:all).with(@klass, :id => [1,2,3]).returns(@records)
+        @returned = @finder.find(@klass, 1,2,3)
+      end
+
+      it "returns the documents" do
+        @returned.should == [@doc, @doc, @doc]
+      end
+    end
+
+    describe "when an object is missing" do
+      before do
+        @datastore.stubs(:all).with(@klass, :id => [1,2,3,4]).returns(@records)
+      end
+
+      it "raises RecordNotFound" do
+        lambda {
+          @finder.find(@klass, 1,2,3,4)
+        }.should raise_error(Friendly::RecordNotFound)
+      end
+    end
+  end
 end
