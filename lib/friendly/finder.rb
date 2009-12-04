8c497a87eb0f6846519f4748a4e9dfd744f83a29
James Golick <james@giraffesoft.ca>
2009-12-04 09:42:58 -0800

    make the finder use the translator

--- lib/friendly/finder.rb
+++ lib/friendly/finder.rb
@@ -8,12 +8,20 @@ module Friendly
     end
 
     def find(klass, id)
-      translator.to_object(klass, datastore.first(klass, :id => id))
+      record = datastore.first(klass, :id => id)
+      assert_record_found(record, klass, id)
+      translator.to_object(klass, record)
     end
 
     protected
       def deserialize(json)
         serializer.parse(json).symbolize_keys
+      end
+
+      def assert_record_found(record, klass, id)
+        if record.nil?
+          raise RecordNotFound, "Couldn't find record #{klass.name}: #{id}."
+        end
       end
   end
 end
--- spec/unit/finder_spec.rb
+++ spec/unit/finder_spec.rb
@@ -1,21 +1,38 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
 describe "Friendly::Finder" do
+  before do
+    @doc        = stub
+    @klass      = stub(:name => "User")
+    @row        = stub
+    @translator = stub
+    @datastore  = stub
+    @finder     = Friendly::Finder.new(@datastore, @translator)
+  end
+
   describe "finding a record by id" do
-    before do
-      @doc        = stub
-      @klass      = stub
-      @row        = stub
-      @translator = stub
-      @translator.stubs(:to_object).with(@klass, @row).returns(@doc)
-      @datastore  = stub
-      @datastore.stubs(:first).with(@klass, :id => 1).returns(@row)
-      @finder     = Friendly::Finder.new(@datastore, @translator)
-      @return     = @finder.find(@klass, 1)
+    describe "when an object is returned" do
+      before do
+        @translator.stubs(:to_object).with(@klass, @row).returns(@doc)
+        @datastore.stubs(:first).with(@klass, :id => 1).returns(@row)
+        @return = @finder.find(@klass, 1)
+      end
+
+      it "returns the translated row" do
+        @return.should == @doc
+      end
     end
 
-    it "returns the translated row" do
-      @return.should == @doc
+    describe "when no object is returned" do
+      before do
+        @datastore.stubs(:first).returns(nil)
+      end
+
+      it "raises RecordNotFound" do
+        lambda {
+          @finder.find(@klass, 1)
+        }.should raise_error(Friendly::RecordNotFound)
+      end
     end
   end
 end
