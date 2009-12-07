1217deb2f627cdf9909ec0b5a7f7f6bbf4217c45
James Golick <james@giraffesoft.ca>
2009-12-07 08:44:03 -0800

    translate documents in to records

--- lib/friendly/translator.rb
+++ lib/friendly/translator.rb
@@ -1,15 +1,30 @@
 module Friendly
   class Translator
-    attr_reader :serializer
+    RESERVED_ATTRS = [:id, :created_at, :updated_at].freeze
 
-    def initialize(serializer = JSON)
+    attr_reader :serializer, :time
+
+    def initialize(serializer = JSON, time = Time)
       @serializer = serializer
+      @time       = time
     end
 
     def to_object(klass, record)
       attributes = serializer.parse(record.delete(:attributes))
       klass.new attributes.merge(record)
     end
+
+    def to_record(document)
+      { :created_at => document.created_at || time.new,
+        :updated_at => time.new,
+        :attributes => serialize(document) }
+    end
+
+    protected
+      def serialize(document)
+        attrs = document.to_hash.reject { |k,v| RESERVED_ATTRS.include?(k) }
+        serializer.generate(attrs)
+      end
   end
 end
 
--- spec/unit/translator_spec.rb
+++ spec/unit/translator_spec.rb
@@ -4,7 +4,8 @@ require 'ostruct'
 describe "Friendly::Translator" do
   before do
     @serializer = stub
-    @translator = Friendly::Translator.new(@serializer)
+    @time       = stub
+    @translator = Friendly::Translator.new(@serializer, @time)
   end
 
   describe "translating a row to an object" do
@@ -27,6 +28,61 @@ describe "Friendly::Translator" do
       @doc.id.should         == 1
       @doc.created_at.should == @time
       @doc.updated_at.should == @time
+    end
+  end
+
+  describe "translating from a document in to a record" do
+    describe "when the document has yet to be saved" do
+      before do
+        @hash = {:name => "Stewie"}
+        @time.stubs(:new).returns(Time.new)
+        @serializer.stubs(:generate).with(@hash).returns("SOME JSON")
+        @document = stub(:to_hash     => @hash, 
+                         :new_record? => true, 
+                         :created_at  => nil)
+        @record = @translator.to_record(@document)
+      end
+
+      it "serializes the attributes" do
+        @record[:attributes].should == "SOME JSON"
+      end
+
+      it "sets a created_at" do
+        @record[:created_at].should == @time.new
+      end
+
+      it "sets updated_at" do
+        @record[:updated_at].should == @time.new
+      end
+    end
+
+    describe "when the document has already been saved" do
+      before do
+        @created_at = Time.new
+        @hash = {:name       => "Stewie",
+                 :id         => 1,
+                 :created_at => @created_at,
+                 :updated_at => Time.new}
+        @time.stubs(:new).returns(Time.new + 5000)
+        @serializer.stubs(:generate).returns("SOME JSON")
+        @document = stub(:to_hash     => @hash, 
+                         :created_at  => @created_at,
+                         :new_record? => false)
+        @record = @translator.to_record(@document)
+      end
+
+      it "serializes the attributes" do
+        @serializer.should have_received(:generate).with(:name => "Stewie")
+        @record[:attributes].should == "SOME JSON"
+      end
+
+      it "doesn't bump the created_at" do
+        @record[:created_at].should == @created_at
+      end
+
+      it "should bump the updated_at" do
+        @record[:updated_at].should == @time.new
+      end
     end
   end
 end
