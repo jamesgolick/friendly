d4c4a71b808beef62eb85ba69ea7e1fc6a0b40b2
James Golick <james@giraffesoft.ca>
2009-12-09 08:44:00 -0800

    ordering spec

--- spec/integration/index_spec.rb
+++ spec/integration/index_spec.rb
@@ -1,4 +1,9 @@
 require File.expand_path("../../spec_helper", __FILE__)
+require 'active_support/duration'
+require 'active_support/core_ext/integer'
+require 'active_support/core_ext/time'
+require 'active_support/core_ext/object'
+require 'active_support/core_ext/numeric'
 
 describe "Finding one object with an index" do
   before do
@@ -23,6 +28,20 @@ describe "Finding object on an index that doesn't exist" do
     lambda {
       User.first(:age => 3) 
     }.should raise_error(Friendly::MissingIndex)
+  end
+end
+
+describe "Finding ordered objects" do
+  before do
+    @three = User.new(:name => "James", :created_at => 4.hours.ago)
+    @two   = User.new(:name => "James", :created_at => 2.hours.ago)
+    @one   = User.new(:name => "James", :created_at => 5.minutes.ago)
+    [@one, @two, @three].each { |i| i.save }
+  end
+
+  it "returns the objects in order" do
+    found_users = User.all(:name => "James", :order! => :created_at.desc)
+    found_users.should == [@one, @two, @three]
   end
 end
 
