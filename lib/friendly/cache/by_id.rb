cf242f81f17b60ed3ee93d839ad2afacbeb58b17
James Golick <james@giraffesoft.ca>
2009-12-13 12:08:58 -0800

    Cache::ByID#first

--- lib/friendly/cache/by_id.rb
+++ lib/friendly/cache/by_id.rb
@@ -2,14 +2,18 @@ module Friendly
   class Cache
     class ByID < Cache
       def store(document)
-        cache.set(cache_key(document), document)
+        cache.set(cache_key(document.id), document)
       end
       alias_method :create, :store
       alias_method :update, :store
 
+      def first(query)
+        cache.get(cache_key(query.conditions[:id]))
+      end
+
       protected
-        def cache_key(document)
-          [klass.name, document.id.to_guid].join("/")
+        def cache_key(id)
+          [klass.name, id.to_guid].join("/")
         end
     end
   end
--- spec/unit/cache_by_id_spec.rb
+++ spec/unit/cache_by_id_spec.rb
@@ -30,4 +30,18 @@ describe "Friendly::Cache::ByID" do
       @cache.should have_received(:set).with("Product/#{@uuid.to_guid}", @doc)
     end
   end
+
+  describe "finding a single object in cache" do
+    describe "when the key is found" do
+      before do
+        @uuid = stub(:to_guid => "xxxx-xxx-xxx-xxxx")
+        @doc  = stub
+        @cache.stubs(:get).with("Product/xxxx-xxx-xxx-xxxx").returns(@doc)
+      end
+
+      it "returns the document" do
+        @id_cache.first(query(:id => @uuid)).should == @doc
+      end
+    end
+  end
 end
