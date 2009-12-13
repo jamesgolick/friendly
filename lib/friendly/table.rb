dd2c707714b0cb8073f68309c0c4b4e3d627abed
James Golick <james@giraffesoft.ca>
2009-12-13 10:26:09 -0800

    factor a Storage subclass out of Table for caches to inherit from

--- lib/friendly/table.rb
+++ lib/friendly/table.rb
@@ -1,32 +1,15 @@
-class Table
-  attr_reader :datastore
+require 'friendly/storage'
+module Friendly
+  class Table < Storage
+    attr_reader :datastore
 
-  def initialize(datastore)
-    @datastore = datastore
-  end
+    def initialize(datastore)
+      @datastore = datastore
+    end
 
-  def table_name
-    raise NotImplementedError, "#{self.class.name}#table_name is not implemented."
-  end
-
-  def create(document)
-    raise NotImplementedError, "#{self.class.name}#create is not implemented."
-  end
-
-  def update(document)
-    raise NotImplementedError, "#{self.class.name}#update is not implemented."
-  end
-
-  def first(conditions)
-    raise NotImplementedError, "#{self.class.name}#first is not implemented."
-  end
-
-  def all(conditions)
-    raise NotImplementedError, "#{self.class.name}#all is not implemented."
-  end
-
-  def satisfies?(conditions)
-    raise NotImplementedError, "#{self.class.name}#satisfies? is not implemented."
+    def table_name
+      raise NotImplementedError, "#{self.class.name}#table_name is not implemented."
+    end
   end
 end
 
