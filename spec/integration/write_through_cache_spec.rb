95e5ef3f7a9d2ba66aad6dd9def7838bda574f04
James Golick <james@giraffesoft.ca>
2009-12-13 12:37:17 -0800

    add integration spec for update and destroy

--- spec/integration/write_through_cache_spec.rb
+++ spec/integration/write_through_cache_spec.rb
@@ -9,3 +9,26 @@ describe "Writing through to cache on create" do
     $cache.get("User/#{@user.id.to_guid}").should == @user
   end
 end
+
+describe "Writing through to cache on update" do
+  before do
+    @user = User.create :name => "Chris"
+    @user.name = "Joe"
+    @user.save
+  end
+  
+  it "writes through to memcache using the model name and guid as cache key" do
+    $cache.get("User/#{@user.id.to_guid}").name.should == @user.name
+  end
+end
+
+describe "Writing through to cache on destroy" do
+  before do
+    @user = User.create :name => "Chris"
+    @user.destroy
+  end
+  
+  it "removes the object from cache" do
+    $cache.get("User/#{@user.id.to_guid}").should be_nil
+  end
+end
