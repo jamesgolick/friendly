3e6569c59a23bf2507cfba9a31b4cb6c965ae8ae
James Golick <james@giraffesoft.ca>
2009-12-19 15:46:44 -0800

    add some integration specs

--- spec/integration/named_scope_spec.rb
+++ spec/integration/named_scope_spec.rb
@@ -5,11 +5,30 @@ describe "named_scope" do
     before do
       User.all(:name => "Quagmire").each { |q| q.destroy }
       5.times { User.create(:name => "Quagmire") }
-      @users = User.named_quagmire
     end
 
-    it "returns all the objects matching the conditions" do
-      @users.should == User.all(:name => "Quagmire")
+    describe "all" do
+      it "returns all objects matching the conditions" do
+        User.named_quagmire.all.should == User.all(:name => "Quagmire")
+      end
+
+      it "accepts extra conditions" do
+        User.create(:name => "Fred")
+        found = User.named_quagmire.all(:name => "Fred")
+        found.should == User.all(:name => "Fred")
+      end
+    end
+
+    describe "first" do
+      it "returns the first object matching the conditions" do
+        User.named_quagmire.first.should == User.first(:name => "Quagmire")
+      end
+
+      it "accepts extra conditions" do
+        User.create(:name => "Fred")
+        found = User.named_quagmire.first(:name => "Fred")
+        found.should == User.first(:name => "Fred")
+      end
     end
   end
 end
