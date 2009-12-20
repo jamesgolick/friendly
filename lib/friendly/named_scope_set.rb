6eab4d3e00416b12917790618771ae3eb5e4d53b
James Golick <james@giraffesoft.ca>
2009-12-19 16:23:22 -0800

    add a proxy and factory for creating and instantiating named_scopes

---
+++ lib/friendly/named_scope_set.rb
@@ -0,0 +1,37 @@
+require 'friendly/named_scope'
+
+module Friendly
+  class NamedScopeSet
+    attr_reader :klass, :named_scope_klass, :scopes
+
+    def initialize(klass, named_scope_klass = NamedScope)
+      @klass             = klass
+      @named_scope_klass = named_scope_klass
+      @scopes            = {}
+    end
+
+    def add(name, parameters)
+      scopes[name] = named_scope_klass.new(klass, parameters)
+      add_scope_method_to_klass(name)
+    end
+
+    def get(name)
+      scopes[name]
+    end
+
+    def get_instance(name)
+      get(name).scope
+    end
+
+    protected
+      def add_scope_method_to_klass(scope_name)
+        klass.class_eval do
+          eval <<-__END__
+            def self.#{scope_name}
+              named_scope_set.get_instance(:#{scope_name})
+            end
+          __END__
+        end
+      end
+  end
+end
