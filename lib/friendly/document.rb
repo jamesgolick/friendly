6f4cf558679c18fb1aceb16154aa55101cbe932c
James Golick <james@giraffesoft.ca>
2009-12-22 11:11:59 -0600

    document Document.has_many

--- lib/friendly/document.rb
+++ lib/friendly/document.rb
@@ -147,6 +147,31 @@ module Friendly
         @association_set ||= Associations::Set.new(self)
       end
 
+      # Add a has_many association.
+      #
+      # e.g.
+      #
+      #     class Post
+      #       attribute :user_id, Friendly::UUID
+      #     end
+      #      
+      #     class User
+      #       has_many :posts
+      #     end
+      #     
+      #     @user = User.create
+      #     @post = Post.create(:user_id => @user.id)
+      #     @user.posts.all == [@post] # => true
+      #
+      # Friendly defaults the foreign key to class_name_id just like ActiveRecord.
+      # It also converts the name of the association to the name of the target class just like ActiveRecord does.
+      #
+      # The biggest difference in semantics between Friendly's has_many and active_record's is that Friendly's just returns a Friendly::Scope object. If you want all the associated objects, you have to call #all to get them. You can also use any other Friendly::Scope method.
+      #
+      # @param [Symbol] name The name of the association and plural name of the target class.
+      # @option options [String] :class_name The name of the target class of this association if it is different than the name would imply.
+      # @option options [Symbol] :foreign_key Override the foreign key.
+      # 
       def has_many(name, options = {})
         association_set.add(name, options)
       end
