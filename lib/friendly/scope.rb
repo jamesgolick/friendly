fa681eb8afeb6cfff3d583c9ea3ff8693f77c908
James Golick <james@giraffesoft.ca>
2009-12-22 12:08:09 -0600

    document Friendly::Scope methods

--- lib/friendly/scope.rb
+++ lib/friendly/scope.rb
@@ -7,22 +7,51 @@ module Friendly
       @parameters = parameters
     end
 
+    # Fetch all documents at this scope.
+    #
+    # @param [Hash] extra_parameters add extra parameters to this query.
+    #
     def all(extra_parameters = {})
       klass.all(params(extra_parameters))
     end
 
+    # Fetch the first document at this scope.
+    #
+    # @param [Hash] extra_parameters add extra parameters to this query.
+    #
     def first(extra_parameters = {})
       klass.first(params(extra_parameters))
     end
 
+    # Paginate the documents at this scope.
+    #
+    # @param [Hash] extra_parameters add extra parameters to this query.
+    # @return WillPaginate::Collection
+    #
     def paginate(extra_parameters = {})
       klass.paginate(params(extra_parameters))
     end
 
+    # Build an object at this scope.
+    #
+    #   e.g.
+    #     Post.scope(:name => "James").build.name # => "James"
+    #
+    # @param [Hash] extra_parameters add extra parameters to this query.
+    #
     def build(extra_parameters = {})
       klass.new(params_without_modifiers(extra_parameters))
     end
 
+    # Create an object at this scope.
+    #
+    #   e.g.
+    #     @post = Post.scope(:name => "James").create
+    #     @post.new_record? # => false
+    #     @post.name # => "James"
+    #
+    # @param [Hash] extra_parameters add extra parameters to this query.
+    #
     def create(extra_parameters = {})
       klass.create(params_without_modifiers(extra_parameters))
     end
