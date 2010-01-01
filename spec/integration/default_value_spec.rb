43f05ed1dc3a065b23db2fa7fab49a54feff3872
James Golick <james@giraffesoft.ca>
2009-12-31 18:14:14 -0600

    update default value spec, since semantics are about to change

--- spec/integration/default_value_spec.rb
+++ spec/integration/default_value_spec.rb
@@ -1,15 +1,30 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
 describe "An attribute with a default value" do
-  before do
-    @user = User.new
+  describe "before saving" do
+    before do
+      @user = User.new
+    end
+
+    it "has the value by default" do
+      @user.happy.should be_true
+    end
+
+    it "has a default vaue even when it's false" do
+      @user.sad.should be_false
+    end
   end
 
-  it "has the value by default" do
-    @user.happy.should be_true
-  end
+  describe "after saving" do
+    before do
+      @user = User.new
+      @user.save
+      @user = User.find(@user.id)
+    end
 
-  it "has a default vaue even when it's false" do
-    @user.sad.should be_false
+    it "doesn't set the existing attributes as dirty" do
+      @user.should_not be_changed
+      @user.should_not be_happy_changed
+    end
   end
 end
