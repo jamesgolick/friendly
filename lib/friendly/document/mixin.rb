9b1b100e4c79fe91bccc711ba07a5fcad5a3287f
James Golick <james@giraffesoft.ca>
2010-01-02 17:40:37 -0600

    only do the special case mixin if we're in a situation where it makes sense to

--- lib/friendly/document/mixin.rb
+++ lib/friendly/document/mixin.rb
@@ -4,7 +4,11 @@ module Friendly
       # FIXME: I'm not in love with this. But, I also don't think it's the
       # end of the world.
       def included(klass)
-        klass.const_get(:ClassMethods).send(:include, self.const_get(:ClassMethods))
+        if klass.const_defined?(:ClassMethods)
+          klass.const_get(:ClassMethods).send(:include, const_get(:ClassMethods))
+        else
+          klass.send(:extend, const_get(:ClassMethods))
+        end
       end
     end
   end
